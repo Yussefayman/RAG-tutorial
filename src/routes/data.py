facc8402127f588f312323712cf1a5519266f196
Yussef Ayman <yussefayman98@gmail.com>
2025-09-20 20:35:12 +0300

    update file upload function

--- src/routes/data.py
+++ src/routes/data.py
@@ -31,7 +31,7 @@ async def upload_data(project_id: str, file: UploadFile,
                         )
                     
                     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-                    file_path = data_controller.generate_unique_filename(orig_file_name=file.filename, project_id=project_id)
+                    file_path, file_id = data_controller.generate_unique_filepath(orig_file_name=file.filename, project_id=project_id)
 
                     try:
                         async with aiofiles.open(file_path,'wb') as f:
@@ -48,7 +48,8 @@ async def upload_data(project_id: str, file: UploadFile,
                     
                     return JSONResponse(
                             content={
-                                "signal":ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                                "signal":ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                                "file_id":file_id
                             }
                         )
 
